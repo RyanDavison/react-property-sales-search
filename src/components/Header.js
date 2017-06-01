369ec438a5fe4ebaa5cf484763926edbf677f6cd
Ryan Davison <ryandavison11@gmail.com>
2017-06-01 15:04:27 -0600

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../images/logo.jpg';
+
+const HeaderBackground = {
+      backgroundColor: "#013E6B",
+      height: "100px",
+      color: "white"
+}
+
+const Header = () =>
+  (
+      <div style={HeaderBackground} className="header">
+          <a href="http://www.mesacounty.us"><img style={{"padding-top" : "0.15em"}} src={logo} className="App-logo" alt="logo" /></a>
+      </div>
+  );
+
+
+export default Header;
