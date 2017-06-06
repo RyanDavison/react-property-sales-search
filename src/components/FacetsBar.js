bbd346ae8feb8f72d2af94477f3336cf470d2fd1
Ryan Davison <ryandavison11@gmail.com>
2017-06-06 11:37:39 -0600

    Dropdown open and close functionality

--- src/components/FacetsBar.js
+++ src/components/FacetsBar.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
-import Datepickers from './Datepickers'
+import React from 'react';
+import Datepickers from './Datepickers';
+import Dropdown from './Dropdown';
+// import Dropdown from 'react-dropdown';
+// import 'react-dropdown/style.css';
+// import '../styles/Dropdown.css';
 
 
 const facetsbar = {
@@ -9,7 +13,7 @@ const facetsbar = {
     boxShadow: "inset 0 6px 2px -2px #e5e5e5",
     boxSizing: "border-box",
     height: "3.2em",
-    minWidth: "890"
+    minWidth: "890px"
 }
 
 const dividerStyle = {
@@ -19,10 +23,37 @@ const dividerStyle = {
     height: "1.5em"
 }
 
+// const dropdownContent = [
+//   {
+//    type: 'group', name: 'Improved Sales', items: [
+//      { value: 'Agricultural', label: 'Agricultural' },
+//      { value: 'Commercial/Industrial', label: 'Commercial/Industrial' },
+//      { value: 'Condo', label: 'Condo' },
+//      { value: 'Duplex/Triplex', label: 'Duplex/Triplex' },
+//      { value: 'Exempt', label: 'Exempt' },
+//      { value: 'Manufactured Home', label: 'Manufactured Home' },
+//      { value: 'Multi Family 4 TO 8', label: 'Multi Family 4 TO 8' },
+//      { value: 'Multi Family 9 AND UP', label: 'Multi Family 9 AND UP' },
+//      { value: 'Single Family', label: 'Single Family' },
+//      { value: 'Townhouse', label: 'Townhouse' }
+//    ]
+//   },
+//   {
+//    type: 'group', name: 'Vacant Sales', items: [
+//      { value: 'Agricultural Vacant', label: 'Agricultural Vacant' },
+//      { value: 'Commercial/Industrial Vacant', label: 'Commercial/Industrial Vacant' },
+//      { value: 'Exempt Vacant', label: 'Exempt Vacant' },
+//      { value: 'Residential Vacant', label: 'Residential Vacant' },
+//    ]
+//   }
+// ]
+
 const FacetsBar = () =>
   (
       <div style={facetsbar} className="facets-bar">
           <Datepickers time={4} units={'months'} />
+          <span style={dividerStyle}></span>
+          <Dropdown placeholder='Select Property Type' value={'Select Property Type'}/>
           <span style={dividerStyle}></span>
       </div>
   );
