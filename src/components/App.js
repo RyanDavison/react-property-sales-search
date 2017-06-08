238c49eb4b6eb0557442cb861b51942a25dcc9f1
Ryan Davison <ryandavison11@gmail.com>
2017-06-08 10:09:29 -0600

    Clean up App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Title from './Title';
 import FacetsBar from './FacetsBar';
 import StartMessage from './StartMessage';
+import Table from './Table';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Title />
           <FacetsBar />
           <StartMessage />
+          <Table />
       </div>
     );
   }
---
+++ src/components/Table.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+  class Table extends Component {
+
+      constructor(props){
+          super(props)
+          this.state = {
+              isShown: false
+          }
+      }
+    render() {
+      return (
+          <table style={{"display": this.state.isShown?"block": "none"}} id="table" class="row-border hover">
+              <thead>
+                  <tr>
+                      <th>Parcel Number (Click for map)</th>
+                      <th>Account Number (Click for details)</th>
+                      <th>Reception No./Book Page (Click for Doc.)</th>
+                      <th>Address</th>
+                      <th>Sale Price</th>
+                      <th>Sale Date</th>
+                      <th>Grantee</th>
+                      <th>Grantor</th>
+                      <th>Actual Value</th>
+                      <th>Qualification Code</th>
+                      <th>Vacant Code</th>
+                      <th>Neighborhood</th>
+                      <th>Economic Area</th>
+                      <th>Property Use</th>
+                      <th>Total Heated Sq Ft</th>
+                      <th>Price by Heated SQFT</th>
+                      <th>Aprox. Acres</th>
+                      <th>Architectual Style 1st Building</th>
+                      <th>Total # Buildings</th>
+                      <th>Earliest Effective Year Built</th>
+                  </tr>
+              </thead>
+              <tbody></tbody>
+          </table>
+      );
+    }
+  }
+
+
+export default Table;
