b17b58ab838a80d761126f60a2c5a58b5f25cb15
Ryan Davison <ryandavison11@gmail.com>
2017-06-15 14:14:40 -0600

    Create test for Title component

--- src/tests/Title.test.js
+++ src/tests/Title.test.js
@@ -2,14 +2,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import sinon from 'sinon';
 import {stubConsoleError} from './utils/test-utils'
-import Qualification from '../components/Qualification';
+import Title from '../components/Title';
 
-describe("<Qualification />", ()=>{
+describe("<Title />", ()=>{
     stubConsoleError();
 
     test('renders without crashing', () => {
         const tree = renderer.create(
-            <Qualification />
+            <Title />
       ).toJSON();
 
       sinon.assert.notCalled(console.error)
