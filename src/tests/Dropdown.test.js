c7c0b8618c844e793b26f32843733ce84814ac62
Ryan Davison <ryandavison11@gmail.com>
2017-06-15 16:23:34 -0600

    Add tests to Dropdown component

--- src/tests/Dropdown.test.js
+++ src/tests/Dropdown.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import sinon from 'sinon';
+import { shallow, mount, render } from 'enzyme';
 import {stubConsoleError} from './utils/test-utils';
 import Dropdown from '../components/Dropdown';
 
@@ -27,7 +28,7 @@ describe("<Dropdown />", ()=>{
       expect(tree).toMatchSnapshot();
     });
 
-    test('UL shows when dropdown clicked', () => {
+    test('UL stays open when a heading is clicked clicked', () => {
         const wrapper = mount(
             <Dropdown
                 baseclass='test-dropdown'
@@ -37,10 +38,74 @@ describe("<Dropdown />", ()=>{
             />
       )
 
-      wrapper.instance().showHide({target:{classname: 'header'}});
-    //   wrapper.instance().changeMax({target:{value: 5000}});
-    //   expect(wrapper.nodes[0].state.minValue).toEqual(500);
-      expect(wrapper.nodes[0].state.isShown).toEqual(5000);
+      //run showHide once with the test-dropdown class to set the display
+      //state to true. Then run showHide to test clicking on a heading.
+      wrapper.instance().showHide({target:{className: 'test-dropdown'}});
+      wrapper.instance().showHide({target:{className: 'heading'}});
+      expect(wrapper.nodes[0].state.display).toEqual(true);
+    });
+
+    test('UL shows or hides when dropdown clicked', () => {
+        const wrapper = mount(
+            <Dropdown
+                baseclass='test-dropdown'
+                option={testContent}
+                placeholder='Select Test Data'
+                value={'Select Test Data'}
+            />
+      )
+
+      wrapper.instance().showHide({target:{className: 'test-dropdown'}});
+      expect(wrapper.nodes[0].state.display).toEqual(true);
+      wrapper.instance().showHide({target:{className: 'test-dropdown'}});
+      expect(wrapper.nodes[0].state.display).toEqual(false);
+    });
+
+    test('UL closes when anything other than dropdown is clicked', () => {
+        const wrapper = mount(
+            <Dropdown
+                baseclass='test-dropdown'
+                option={testContent}
+                placeholder='Select Test Data'
+                value={'Select Test Data'}
+            />
+      )
+
+      wrapper.instance().showHide({target:{className: 'test-dropdown'}});
+      expect(wrapper.nodes[0].state.display).toEqual(true);
+      wrapper.instance().showHide({target:{className: 'some-random-className'}});
+      expect(wrapper.nodes[0].state.display).toEqual(false);
+    });
+
+    test('UL closes when a list item is clicked', () => {
+        const wrapper = mount(
+            <Dropdown
+                baseclass='test-dropdown'
+                option={testContent}
+                placeholder='Select Test Data'
+                value={'Select Test Data'}
+            />
+      )
+
+      wrapper.instance().showHide({target:{className: 'test-dropdown'}});
+      expect(wrapper.nodes[0].state.display).toEqual(true);
+      wrapper.instance().showHide({target:{className: 'item'}});
+      expect(wrapper.nodes[0].state.display).toEqual(false);
+    });
+
+    test('Test selectNode method to change the state of selected value', () => {
+        const wrapper = mount(
+            <Dropdown
+                baseclass='test-dropdown'
+                option={testContent}
+                placeholder='Select Test Data'
+                value={'Select Test Data'}
+            />
+      )
+
+      wrapper.instance().selectNode({target:{className: 'test-dropdown',
+                                    innerHTML:'Agricultural'}});
+      expect(wrapper.nodes[0].state.value).toEqual('Agricultural');
     });
 
 });
