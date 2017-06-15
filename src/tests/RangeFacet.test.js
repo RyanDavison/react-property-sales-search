3ce49484daa8669785e4d995dc20503ada09fa3b
Ryan Davison <ryandavison11@gmail.com>
2017-06-15 07:38:19 -0600

    Create RangeFacet component test

--- src/tests/RangeFacet.test.js
+++ src/tests/RangeFacet.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import RangeFacet from '../components/RangeFacet';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<RangeFacet />, div);
 });
