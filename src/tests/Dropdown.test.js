e4058ea16348a63b22bb566b8d89cc8738566454
Ryan Davison <ryandavison11@gmail.com>
2017-06-15 15:15:36 -0600

    Add props to test Dropdosn component

--- src/tests/Dropdown.test.js
+++ src/tests/Dropdown.test.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import sinon from 'sinon';
-// import {stubConsoleError} from './utils/test-utils';
+import {stubConsoleError} from './utils/test-utils';
 import Dropdown from '../components/Dropdown';
 
 describe("<Dropdown />", ()=>{
-    // stubConsoleError();
-    //
-    // test('renders without crashing', () => {
-    //     const tree = renderer.create(
-    //         <Dropdown />
-    //   ).toJSON();
-    //
-    //   sinon.assert.notCalled(console.error)
-    //   expect(tree).toMatchSnapshot();
-    // });
-    //
+    stubConsoleError();
+
+    const testContent = [
+         {type: 'heading', value: 'Improved Sales', label: 'Improved Sales'},
+         {type: 'item', value: 'Agricultural', label: 'Agricultural' },
+         {type: 'item', value: 'Commercial/Industrial', label: 'Commercial/Industrial' },
+         {type: 'item', value: 'Agricultural Vacant', label: 'Agricultural Vacant' },
+    ]
+
+    test('renders without crashing', () => {
+        const tree = renderer.create(
+            <Dropdown
+                baseclass='test-dropdown'
+                option={testContent}
+                placeholder='Select Test Data'
+                value={'Select Test Data'}/>
+      ).toJSON();
+
+      sinon.assert.notCalled(console.error)
+      expect(tree).toMatchSnapshot();
+    });
+
     // test('UL shows when dropdown clicked', () => {
     //     const tree = renderer.create(
     //         <Dropdown />
