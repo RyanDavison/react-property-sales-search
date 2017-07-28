d22f5ad966b94dce61e047439b678dd2f9a0859b
Ryan Davison <ryandavison11@gmail.com>
2017-07-28 08:11:29 -0600

    Remove useless call to axios

--- src/components/BufferFacet/BufferFacet.js
+++ src/components/BufferFacet/BufferFacet.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import RangeFacetLabel from '../RangeFacetLabel';
 import Autocomplete from '../Autocomplete';
 import ClearButton from '../ClearButton';
