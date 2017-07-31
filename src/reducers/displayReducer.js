80d7a8f1d609392dc1f2a6ea5c662f5f302ca08a
Ryan Davison <ryandavison11@gmail.com>
2017-07-31 12:19:28 -0600

    Add spinner display to initial state

--- src/reducers/displayReducer.js
+++ src/reducers/displayReducer.js
@@ -3,7 +3,8 @@ import * as actions from '../actions/actionTypes';
 initDisplayState = {
     modalIsOpen: false,
     startMessageIsOpen: true,
-    tableIsOpen:false
+    tableIsOpen:false,
+    loadingSpinnerIsOpen: false
 }
 
 export default function displayReducer(state = initDisplayStates, action){
@@ -11,9 +12,9 @@ export default function displayReducer(state = initDisplayStates, action){
         case actions.TOGGLE_MODAL:
             return {modalIsOpen: action.modalIsOpen};
         case actions.TOGGLE_START_MESSAGE
-            return {startMessageIsOpen: action.startMessageIsOpen}
+            return {startMessageIsOpen: action.startMessageIsOpen};
         case actions.TOGGLE_TABLE
-            return {tableIsOpen: action.tableIsOpen}
+            return {tableIsOpen: action.tableIsOpen};
         default:
             return state;
     }
