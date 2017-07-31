1dc9cd94ddebe94f070c13810656fefa7cd0057c
Ryan Davison <ryandavison11@gmail.com>
2017-07-31 11:27:57 -0600

    Add startMessageIsOpen and tableIsOpen to global Redux state

--- src/reducers/displayReducer.js
+++ src/reducers/displayReducer.js
@@ -1,9 +1,19 @@
 import * as actions from '../actions/actionTypes';
 
-export default function displayReducer(state = {modalIsOpen: false}, action){
+initDisplayState = {
+    modalIsOpen: false,
+    startMessageIsOpen: true,
+    tableIsOpen:false
+}
+
+export default function displayReducer(state = initDisplayStates, action){
     switch(action.type){
         case actions.TOGGLE_MODAL:
             return {modalIsOpen: action.modalIsOpen};
+        case actions.TOGGLE_START_MESSAGE
+            return {startMessageIsOpen: action.startMessageIsOpen}
+        case actions.TOGGLE_TABLE
+            return {tableIsOpen: action.tableIsOpen}
         default:
             return state;
     }
