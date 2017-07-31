16a0feabc198c1b0f496d27d007a28ebdfa8aea0
Ryan Davison <ryandavison11@gmail.com>
2017-07-31 13:37:00 -0600

    Fix switch statement syntax

--- src/reducers/displayReducer.js
+++ src/reducers/displayReducer.js
@@ -1,6 +1,6 @@
 import * as actions from '../actions/actionTypes';
 
-initDisplayState = {
+const initDisplayState = {
     modalIsOpen: false,
     startMessageIsOpen: true,
     tableIsOpen:false,
@@ -11,9 +11,9 @@ export default function displayReducer(state = initDisplayStates, action){
     switch(action.type){
         case actions.TOGGLE_MODAL:
             return {modalIsOpen: action.modalIsOpen};
-        case actions.TOGGLE_START_MESSAGE
+        case actions.TOGGLE_START_MESSAGE:
             return {startMessageIsOpen: action.startMessageIsOpen};
-        case actions.TOGGLE_TABLE
+        case actions.TOGGLE_TABLE:
             return {tableIsOpen: action.tableIsOpen};
         default:
             return state;
